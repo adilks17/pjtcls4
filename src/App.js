cf1afa00feafb1218017050147c2b100f56c8c75
Akashkp-01 <127194715+Akashkp-01@users.noreply.github.com>
2023-10-06 15:44:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import Transferdata from "./Components/Transferdata";
+// import Login from "./Components/Login";
+// import Course from "./Components/Course/Course";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
+import Logindata from "./Components/Logindata";
+import Course from "./Components/Course/Course";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Transferdata username="abc" password="xyz" usertype="admin"/> */}
+      {/* <Login/> */}
+      {/* <Course/> */}
+     
+     <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Login />}></Route>
+          <Route path={'/Ldata'} element={<Logindata/>}></Route>
+          <Route path={'/Course'} element={<Course />}></Route>
+ 
+        </Routes>
+     </BrowserRouter>
+
     </div>
   );
 }
